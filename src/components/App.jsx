5a016ae5cf584fc931b3a9b1a0357b6f16ed99d2
Michael Skeen <mmskeen@gmail.com>
2020-03-25 17:07:27 -0600

    Finished exercise

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,22 +1,15 @@
 import React from "react";
+import Card from "./Card";
+import contacts from "./contacts";
 
 function App() {
+  const cards = contacts.map(c => (
+    <Card name={c.name} img={c.imgURL} tel={c.phone} email={c.email} />
+  ));
   return (
     <div>
       <h1 className="heading">My Contacts</h1>
-      <div className="card">
-        <div className="top">
-          <h2>Beyonce</h2>
-          <img
-            src="https://blackhistorywall.files.wordpress.com/2010/02/picture-device-independent-bitmap-119.jpg"
-            alt="avatar_img"
-          />
-        </div>
-        <div className="bottom">
-          <p>+123 456 789</p>
-          <p>b@beyonce.com</p>
-        </div>
-      </div>
+      {cards}
     </div>
   );
 }
